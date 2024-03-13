e2edee9cac474f3b6b578bfa30a46307116450da
RAGUNATH-R-30 <ragunath3003@gmail.com>
2024-03-13 16:52:37 +0530

    navbuttonchange

--- src/nav_component.jsx
+++ src/nav_component.jsx
@@ -13,7 +13,7 @@ function Navbar({ modaldisplay, cartcount }) {
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon" onClick={() => { menutoggler(!menutoggle) }}></span>
                 </button>
-                <div className={`collapse navbar-collapse show}`} id="navbarSupportedContent">
+                <div className={`collapse navbar-collapse ${menutoggle?"show":null}`} id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
                             <a className="nav-link active" aria-current="page" href="#">Home</a>
